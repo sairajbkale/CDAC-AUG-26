881028f87a38323be0036cd13a6303fdc1f6e151
Sairaj Kale_AC <sairaj.kale.cmaug26@gmail.com>
2026-08-21 00:26:16 +0530

    Upload Even No. Program

--- Assignments/Logic Building/Assignment3/Problem 3/EvenNo.java
+++ Assignments/Logic Building/Assignment3/Problem 3/EvenNo.java
@@ -1,1 +1,15 @@
-
+class EvenNo{
+	public static void main(String []args){
+		EvenNo en = new EvenNo();
+		en.printEvenNumbers();
+	}
+	void printEvenNumbers(){
+		int i = 1;
+			while(i<=50){
+				if(i%2==0){
+					System.out.println(i);
+				}
+				i++;
+			}
+	}
+}
