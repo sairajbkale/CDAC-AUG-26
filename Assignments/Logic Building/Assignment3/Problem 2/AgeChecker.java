83ecd30be07ef59edde9683650e3247a4f614b87
Sairaj Kale_AC <sairaj.kale.cmaug26@gmail.com>
2026-08-21 00:24:55 +0530

    Upload Age Checker Program

--- Assignments/Logic Building/Assignment3/Problem 2/AgeChecker.java
+++ Assignments/Logic Building/Assignment3/Problem 2/AgeChecker.java
@@ -1,1 +1,22 @@
+import java.util.*;
 
+class AgeChecker{
+	public static void main(String []args){
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the Age");
+		int age = sc.nextInt();
+		AgeChecker ac = new AgeChecker();
+		System.out.println("The person is "+ac.checkAgeCategory(age));
+	}
+	String checkAgeCategory(int age){
+		if(age<18){
+			return "Minor";
+		}
+		else if(age>=18 && age<60){
+			return "Ault";
+		}
+		else {
+			return "Senior Citizen";
+		}
+	}
+}
