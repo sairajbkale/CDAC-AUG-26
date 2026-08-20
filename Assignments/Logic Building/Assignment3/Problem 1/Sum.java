7f1b4633cf2d69e214be08b6ff39dace968cb68f
Sairaj Kale_AC <sairaj.kale.cmaug26@gmail.com>
2026-08-21 00:23:45 +0530

    Uplod Sum of 2 No. Program

--- Assignments/Logic Building/Assignment3/Problem 1/Sum.java
+++ Assignments/Logic Building/Assignment3/Problem 1/Sum.java
@@ -1,1 +1,16 @@
+import java.util.*;
+class Sum{
+		public static void main(String []args){
+			Scanner sc = new Scanner(System.in);
+			System.out.println("Enter the first number");
+			int num1 = sc.nextInt();
+			System.out.println("Enter the second number");
+			int num2 = sc.nextInt();
+			Sum s = new Sum();
+			System.out.println("The Sum is "+s.sumOfTwoNumbers(num1,num2));
+		}
 
+int sumOfTwoNumbers(int num1,int num2){
+	return num1 + num2;
+}
+}
