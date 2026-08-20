d31ccba641c8b620e1dd3f543c62282dbc316f4a
Sairaj Kale_AC <sairaj.kale.cmaug26@gmail.com>
2026-08-21 00:28:03 +0530

    User Input for Positive Number Program

--- Assignments/Logic Building/Assignment3/Problem 4/PositiveNo.java
+++ Assignments/Logic Building/Assignment3/Problem 4/PositiveNo.java
@@ -1,1 +1,17 @@
-
+import java.util.*;
+class PositiveNo{
+	public static void main(String []args){
+			
+			PositiveNo pn = new PositiveNo();
+			pn.askForPositiveNumber();
+	}
+	void askForPositiveNumber(){
+		Scanner sc = new Scanner(System.in);
+		int i = 0;
+		do{
+			System.out.println("Enter the number: ");
+			i = sc.nextInt();
+		}while(i<=0);
+		System.out.println("The positive number is "+i);
+}
+}
