dfbad0f8eda6b6ea2779810342a0af50e4fa587b
Sairaj Kale_AC <sairaj.kale.cmaug26@gmail.com>
2026-08-21 00:31:42 +0530

    Program to Caluclate Sum of Numbers from 1 to N

--- Assignments/Logic Building/Assignment3/Problem 6/SumOfNumber.java
+++ Assignments/Logic Building/Assignment3/Problem 6/SumOfNumber.java
@@ -1,1 +1,17 @@
-
+import java.util.*;
+class SumOfNumber{
+	public static void main(String []args){
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter the number ");
+		int num = sc.nextInt();
+		SumOfNumber sn = new SumOfNumber();
+		System.out.println("The sum of number is "+sn.calculateSum(num));
+	}
+	int calculateSum(int num){
+		int i=0;
+		for(int j=0;j<=num;j++){
+			i = i+j;
+		}
+		return i;
+	}
+}
