d0ba959a1c38793f4dd5ceeeaebcd69e7fc502f9
Sairaj Kale_AC <sairaj.kale.cmaug26@gmail.com>
2026-08-21 00:29:31 +0530

    Program to Print Multiplication Table

--- Assignments/Logic Building/Assignment3/Problem 5/Multiplication.java
+++ Assignments/Logic Building/Assignment3/Problem 5/Multiplication.java
@@ -1,1 +1,15 @@
-
+import java.util.*;
+class Multiplication{
+	public static void main(String []args){
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Print the table of Number: ");
+		int num = sc.nextInt();
+		Multiplication m = new Multiplication();
+		m.printMultiplicationTable(num);
+	}
+	void printMultiplicationTable(int num){
+		for(int i=1;i<=10;i++){
+			System.out.println(num + "x" + i + "=" + num*i);
+		}
+	}
+}
